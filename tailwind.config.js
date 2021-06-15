893066caa0e44b4be5165989f68ce02e0e85bcf8
Quinteiros <agrovila.quinteiros@gmail.com>
2021-06-16 00:32:47 +0100

    Updated tailwind config js with tailwindcss ui, forms, aspect-ratio and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,37 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
-    purge: [
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  purge: [],
+
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+
+  purge: [
+    './Modules/**/*.blade.php',
+    './Modules/**/*.js',
+    './Modules/**/*.vue',
+  ],
+
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+          sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
     },
-
-    variants: {
-        opacity: ['responsive', 'hover', 'focus', 'disabled'],
-    },
-
-    plugins: [require('@tailwindcss/ui')],
-};
+  },
+  variants: {
+    extend: {},
+  },
+  
+  plugins: [
+    // ...
+    require('@tailwindcss/ui'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography')
+  ],
+}
