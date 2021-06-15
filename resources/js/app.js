6d357a0e21f07de9f2abc494bf4f9aed65470712
Quinteiros <agrovila.quinteiros@gmail.com>
2021-06-15 18:37:35 +0100

    Added tinymce in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,12 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+tinymce.init({
+    selector:'textarea.editor',
+    forced_root_block : "",
+    selector:'textarea',
+    relative_urls: true,
+    fullpage_default_encoding: 'UTF-8',
+    plugins: 'link',
+});
