995b267415c8dbb7397c023492d83e857d327417
Quinteiros <agrovila.quinteiros@gmail.com>
2021-06-15 23:24:27 +0100

    Updated translations lang files from https://github.com/Laravel-Lang/lang/blob/master/

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset'     => 'Votre mot de passe a &eacute;t&eacute; r&eacute;initialis&eacute; !',
+    'sent'      => 'Nous vous avons envoy&eacute; par e-mail le lien de r&eacute;initialisation du mot de passe !',
+    'throttled' => 'Veuillez patienter avant de r&eacute;essayer.',
+    'token'     => 'Ce jeton de r&eacute;initialisation du mot de passe n\'est pas valide.',
+    'user'      => 'D&eacute;sol&eacute;&nbsp;! Aucun utilisateur avec cette adresse e-mail.',
+];
