d09db7aee4b3c5a1d22a6a28932eefd30cf0d474
Quinteiros <agrovila.quinteiros@gmail.com>
2021-06-15 19:06:39 +0100

    Verified routes and redirections

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,29 @@ Route::group(['middleware' => 'language'], function () {
         return view('welcome');
     });
 
+    /*******************
+    | * REGISTER & VERIFY *
+    | ******************/
+    
+    
+    Route::get('/email/verify', function () {
+        return view('auth.verify-email');
+    })->middleware('auth')->name('verification.notice');
+    
+    
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+    
+        return redirect('/home');
+    })->middleware(['auth', 'signed'])->name('verification.verify');
+    
+    
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+    
+        return back()->with('message', 'Verification link sent!');
+    })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
     Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
